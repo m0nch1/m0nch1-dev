314d88e904d86f13779d04454b948331a77803d6
m0nch1 <yuta.kato@0004s.com>
2022-05-30 00:09:07 +0900

    wip: atropos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind()]
+  integrations: [solid(), vue(), tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ["atropos"],
+    },
+  },
 });
---
+++ src/components/pages/home/Atropos/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <atropos class="atropos atropos-header">
+    <img data-atropos-offset="-4.5" src="/i/atropos-bg.svg" alt="" />
+    <img src="/m0nch1_face.png" data-atropos-offset="10" />
+  </atropos>
+</template>
+<script>
+import Atropos from "atropos/vue";
+
+export default {
+  components: {
+    Atropos,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "atropos/scss";
+
+.atropos {
+  width: 50%;
+  height: 300px;
+}
+</style>
