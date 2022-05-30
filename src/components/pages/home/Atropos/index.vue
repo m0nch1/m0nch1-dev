9968ab022699dc12eb83568ce75775e22e901857
m0nch1 <yuta.kato@0004s.com>
2022-05-30 10:33:22 +0900

    wip: atropos 仮設置

--- src/components/pages/home/Atropos/index.vue
+++ src/components/pages/home/Atropos/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <atropos class="atropos atropos-header">
+  <atropos
+    class="atropos max-w-screen-lg mx-auto pt-32 pb-16 px-4 md:px-8 lg:px-16"
+  >
     <img data-atropos-offset="-4.5" src="/i/atropos-bg.svg" alt="" />
-    <img src="/m0nch1_face.png" data-atropos-offset="10" />
+    <img data-atropos-offset="-2.5" src="/i/atropos-mountains.svg" alt="" />
+    <img data-atropos-offset="0" src="/i/atropos-forest-back.svg" alt="" />
+    <img data-atropos-offset="2" src="/i/atropos-forest-mid.svg" alt="" />
+    <img data-atropos-offset="4" src="/i/atropos-forest-front.svg" alt="" />
+    <img data-atropos-offset="5" src="/i/atropos-logo-en.svg" alt="" />
   </atropos>
 </template>
 <script>
@@ -15,10 +21,22 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import "atropos/scss";
-
 .atropos {
-  width: 50%;
+  width: 100%;
   height: 300px;
+
+  img {
+    position: absolute;
+    left: -5%;
+    top: -5%;
+    width: 110%;
+    height: 110%;
+    object-fit: contain;
+    display: block;
+    z-index: 1;
+    transform-style: preserve-3d;
+    pointer-events: none;
+    max-width: none;
+  }
 }
 </style>
