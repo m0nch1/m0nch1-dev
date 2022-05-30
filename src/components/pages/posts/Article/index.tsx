8cd620aef13881ac6d06d74661b8acbd7a320a7a
m0nch1 <yuta.kato@0004s.com>
2022-05-30 23:48:58 +0900

    微修正

--- src/components/pages/posts/Article/index.tsx
+++ src/components/pages/posts/Article/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Article = (props: Props) => {
   return (
     <div class="flex flex-col md:flex-row items-center gap-4 lg:gap-6">
-      <a href="#" class="group w-full md:w-24 lg:w-40 h-56 md:h-24 lg:h-40 block self-start shrink-0 bg-gray-100 overflow-hidden rounded-lg shadow-lg relative">
+      <a href={props.url} class="group w-full md:w-24 lg:w-40 h-56 md:h-24 lg:h-40 block self-start shrink-0 bg-gray-100 overflow-hidden rounded-lg shadow-lg relative">
         <img src={props.thumbnailUrl} loading="lazy" alt="Photo by Minh Pham" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
       </a>
 
@@ -17,7 +17,9 @@ const Article = (props: Props) => {
         <span class="text-gray-400 text-sm">{props.firstPublishedAt}</span>
 
         <h2 class="text-gray-800 text-xl font-bold">
-          <span>{props.title}</span>
+          <a href={props.url}>
+            <span>{props.title}</span>
+          </a>
         </h2>
 
         <p class="text-gray-500">{props.description}</p>
